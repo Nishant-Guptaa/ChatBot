94b9a7535318fce348e2b7d252119c5409f07e0c
Nishant Kumar <nishant098097@gmail.com>
2025-04-13 10:55:26 +0530

    Fix: Updated Port configration

--- client/src/components/ChatInterface.js
+++ client/src/components/ChatInterface.js
@@ -137,7 +137,7 @@ const ChatInterface = () => {
     setIsLoading(true);
 
     try {
-      const response = await fetch('http://localhost:5000/api/chat', {
+      const response = await fetch('http://localhost:10000/api/chat', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
